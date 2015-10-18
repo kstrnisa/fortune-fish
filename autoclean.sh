31f74d7aeeda439d51ced82ad1d45dacdcb54483
Klemen Strnisa <klemen.strnisa@esss.se>
2015-10-18 18:39:32 +0200

    Remove autogenerated INSTALL file

--- autoclean.sh
+++ autoclean.sh
@@ -7,4 +7,4 @@ rm po/Makefile.in.in
 
 rm -rf m4 autom4te.cache
 
-rm aclocal.m4 compile configure config.guess config.h.in config.sub configure.ac depcomp install-sh ltmain.sh missing
+rm aclocal.m4 compile configure config.guess config.h.in config.sub configure.ac depcomp install-sh ltmain.sh missing INSTALL
